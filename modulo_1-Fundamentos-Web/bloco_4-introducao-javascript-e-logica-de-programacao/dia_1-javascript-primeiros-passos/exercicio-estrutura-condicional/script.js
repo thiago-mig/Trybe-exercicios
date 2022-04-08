3d2aad42324b5e041d0519664fd871cf6c85f92c
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-04-07 23:22:03 -0300

    Agora incluindo todos os exercícios no repositório

---
+++ modulo_1-Fundamentos-Web/bloco_4-introducao-javascript-e-logica-de-programacao/dia_1-javascript-primeiros-passos/exercicio-estrutura-condicional/script.js
@@ -0,0 +1,11 @@
+const nota = 10;
+
+if (nota >= 80){
+    console.log('Parabéns, você foi aprovado!');
+}
+else if (nota >= 60 && nota <= 80){
+    console.log('Você está na nossa lista de espera.');
+}
+else{
+    console.log('Você foi reprovado');
+}
