e1e5cedcd11aa69dd019e9ed5f4f905ccd2d56c6
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-04-18 21:44:44 -0300

    Colocando os exercícios atrasados em dia.

--- modulo_1-Fundamentos-Web/bloco_4-introducao-javascript-e-logica-de-programacao/dia_1-javascript-primeiros-passos/exercicio-final-do-dia/script7.js
+++ modulo_1-Fundamentos-Web/bloco_4-introducao-javascript-e-logica-de-programacao/dia_1-javascript-primeiros-passos/exercicio-final-do-dia/script7.js
@@ -1,17 +1,22 @@
 let nota = 86;
-let conceito;
-if (nota >= 90 && nota<= 100){
-    conceito = 'A';
-}else if (nota <= 90 && nota >= 80) {
-    conceito = 'B';
-}else if (nota <= 80 && nota >= 70){
-    conceito = 'C';
-}else if (nota <= 70 && nota >= 60){
-    conceito = 'D';
-}else if (nota <= 60 && nota >= 50){
-    conceito = 'E';
-}else{
-    console.log('Nota inválida');
+function conceito(nota) {
+    let conceito;
+    if (nota >= 90 && nota <= 100) {
+        conceito = 'A';
+    } else if (nota <= 90 && nota >= 80) {
+        conceito = 'B';
+    } else if (nota <= 80 && nota >= 70) {
+        conceito = 'C';
+    } else if (nota <= 70 && nota >= 60) {
+        conceito = 'D';
+    } else if (nota <= 60 && nota >= 50) {
+        conceito = 'E';
+    } else if (nota < 50) {
+        conceito = 'F';
+    } else {
+        conceito = 'Nota inválida'
+    }
+    return conceito;
 }
 
 console.log(conceito);
