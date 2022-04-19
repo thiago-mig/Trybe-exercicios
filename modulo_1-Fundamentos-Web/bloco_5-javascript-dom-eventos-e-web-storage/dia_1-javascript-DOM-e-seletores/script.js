f749b97f949fefbfd4c9b1d2c96de46de68c833e
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-04-19 17:52:09 -0300

    Finalizando exercícios de seletores DOM

---
+++ modulo_1-Fundamentos-Web/bloco_5-javascript-dom-eventos-e-web-storage/dia_1-javascript-DOM-e-seletores/script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('#header-container')[0].style.backgroundColor = 'green';
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'purple';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'purple';
+document.querySelectorAll('.emergency-tasks div')[0].style.backgroundColor = 'salmon';
+document.querySelectorAll('.emergency-tasks div')[1].style.backgroundColor = 'salmon';
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks div')[0].style.backgroundColor = 'yellow';
+document.querySelectorAll('.no-emergency-tasks div')[1].style.backgroundColor = 'yellow';
+
+document.querySelectorAll('#footer-container div')[0].style.backgroundColor= 'darkgreen';
