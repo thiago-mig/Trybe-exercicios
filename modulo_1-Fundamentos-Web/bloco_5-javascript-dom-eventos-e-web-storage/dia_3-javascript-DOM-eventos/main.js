27617196834f1be3c183e045ebfe17f4456f620c
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-04-21 23:29:14 -0300

    Realizando o 4 quesito

--- modulo_1-Fundamentos-Web/bloco_5-javascript-dom-eventos-e-web-storage/dia_3-javascript-DOM-eventos/main.js
+++ modulo_1-Fundamentos-Web/bloco_5-javascript-dom-eventos-e-web-storage/dia_3-javascript-DOM-eventos/main.js
@@ -33,7 +33,12 @@ input.addEventListener('input', function(){digitarEmTech(this);});
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-
+// function levaPraPagina(){
+    //window.location.replace('https://blog.betrybe.com/');
+//}
+myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://blog.betrybe.com/');
+  });
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
