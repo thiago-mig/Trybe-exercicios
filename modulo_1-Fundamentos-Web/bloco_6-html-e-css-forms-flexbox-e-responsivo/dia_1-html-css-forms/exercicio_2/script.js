af4b3f6f2cef07959275ceb0c6f25ea6d3aed9f6
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-05-05 09:52:04 -0300

    Resolvendo o exercício introduzindo os conceitos do evento preventDefault()

--- modulo_1-Fundamentos-Web/bloco_6-html-e-css-forms-flexbox-e-responsivo/dia_1-html-css-forms/exercicio_2/script.js
+++ modulo_1-Fundamentos-Web/bloco_6-html-e-css-forms-flexbox-e-responsivo/dia_1-html-css-forms/exercicio_2/script.js
@@ -5,4 +5,15 @@ const HREF_LINK = document.querySelector("#href");
 
 HREF_LINK.addEventListener('click', function (evento) {
     evento.preventDefault();
-})
+});
+
+INPUT_CHECKBOX.addEventListener('click', function (evento){
+    evento.preventDefault();
+});
+
+INPUT_TEXT.addEventListener('keypress', function (evento){
+    const tecla = evento.key;
+    if (tecla !== 'a') {
+        evento.preventDefault();
+    }
+});
