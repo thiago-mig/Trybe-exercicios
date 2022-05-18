ad297508cbeec4924726be0d70b5cb197710842d
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-05-17 23:24:59 -0300

    Realizando o terceiro quesito

--- modulo_1-Fundamentos-Web/bloco_7-introducao-a-javascript-es6-e-testes-unitarios/dia_3-primeiros-passos-em-jest/exercicio-final/funcoes.test.js
+++ modulo_1-Fundamentos-Web/bloco_7-introducao-a-javascript-es6-e-testes-unitarios/dia_3-primeiros-passos-em-jest/exercicio-final/funcoes.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./funcoes.js');
+const {sum, myRemove, myFizzBuzz,} = require('./funcoes.js');
 
 //🚀 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
 
@@ -22,6 +22,7 @@ describe('Função que realiza a soma de 2 parâmetros:', () => {
   });
 
 });
+
 //🚀 2 - A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
 
 describe('2 - A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array', () => {
@@ -37,3 +38,21 @@ describe('2 - A função myRemove(arr, item) recebe um array arr e retorna uma c
 
 });
 
+describe('🚀 3 - A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5, retorna "fizz" se for divisível apenas por 3, retorna "buzz" se divisível apenas por 5, retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número', () => {
+    it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () =>{
+        expect(myFizzBuzz(15)).toBe('fizzbuzz');
+    });
+    it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(3)).toBe('fizz');
+    });
+    it('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(5)).toBe('buzz');
+    });
+    it('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () =>{
+        expect(myFizzBuzz(4)).toBe(4);
+    });
+    it('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz('5')).toBe(false);
+    });
+
+});
