d517c180f5f908a237ccad1cef421b2e5414596e
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-05-24 23:05:54 -0300

    Realizando os primeiros exercícios de some e every

--- modulo_1-Fundamentos-Web/bloco_8-higher-order-functions-do-javascript-es6/dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-de-ambientação/script.js
+++ modulo_1-Fundamentos-Web/bloco_8-higher-order-functions-do-javascript-es6/dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-de-ambientação/script.js
@@ -55,4 +55,32 @@ const musicas = [
     return musica;
   }
   
-  console.log(findMusic('31031685'))
+  console.log(findMusic('31031685'));
+
+//1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false, use some;
+
+const names2 = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  return arr.some((element) => element === name);
+}
+
+console.log(hasName(names2, 'Ana'));
+
+// 2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false, use every;
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = (arr, minimumAge) => {
+    //Adicione seu código aqui
+    return arr.every((element) => element.age === minimumAge);
+  }
+  
+  console.log(verifyAges(people, 18));
