2c304cc0991c8bfec548046ebd9c6e3a2de78f5b
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-05-24 22:50:40 -0300

    Realizando os primeiros exercícios de find

--- modulo_1-Fundamentos-Web/bloco_8-higher-order-functions-do-javascript-es6/dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-de-ambientação/script.js
+++ modulo_1-Fundamentos-Web/bloco_8-higher-order-functions-do-javascript-es6/dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-de-ambientação/script.js
@@ -13,3 +13,46 @@ const emailListInData = [
   
   // Adicione seu código aqui
   emailListInData.forEach(showEmailList);
+
+//   1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  const x = numbers.find((element) => element % 3 === 0 && element % 5 === 0);
+  return x;
+};
+
+console.log(findDivisibleBy3And5());
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  const nomeCom5Letras = names.find((element) => element.length === 5);
+  return nomeCom5Letras;
+}
+
+console.log(findNameWithFiveLetters());
+
+// 3 - Utilize o find para encontrar a música com id igual a 31031685, caso ela exista:
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+    // Adicione seu código aqui
+    let musica;
+    musicas.find((element) => {
+      if(element.id === id) {
+        musica = element.title;
+      }
+    });
+    return musica;
+  }
+  
+  console.log(findMusic('31031685'))
