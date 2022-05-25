240213d0cfcef911ec4fde8820b190906e424635
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2022-05-24 23:17:20 -0300

    Realizando o exercício de função sort

--- modulo_1-Fundamentos-Web/bloco_8-higher-order-functions-do-javascript-es6/dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-de-ambientacao/script.js
+++ modulo_1-Fundamentos-Web/bloco_8-higher-order-functions-do-javascript-es6/dia_2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-de-ambientacao/script.js
@@ -84,3 +84,17 @@ const people = [
   }
   
   console.log(verifyAges(people, 18));
+
+//   1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente.
+const people2 = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  // Adicione se código aqui
+  people2.sort((a, b) => a.age - b.age);
+  
+  console.log(people2);
